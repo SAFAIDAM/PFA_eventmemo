a6b60f034772cbe08880836b2adf4a95f25c15b3
safa idam <idamhamedsafa@gmail.com>
2023-06-19 10:22:54 +0100

    finall

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ from flask_login import UserMixin, LoginManager, login_required, login_user, log
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from flask_migrate import Migrate
-from webforms import Login, EditProfile, EditEventForm, Sign_up, PostForm
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError, TextAreaField, FileField, DateTimeField, DateTimeLocalField
+from wtforms.validators import InputRequired, Length, ValidationError, DataRequired, EqualTo, length
+from wtforms.widgets import TextArea
 from werkzeug.utils import secure_filename
 import uuid as uuid
 import os
@@ -315,6 +318,65 @@ def create_app():
     @app.errorhandler(500)
     def page_not_found(e):
         return render_template("500.html"), 500
+    
+
+        
+
+
+
+
+    class Login(FlaskForm):
+        email = StringField('EMAIL', validators=[DataRequired()])
+        password = PasswordField('PASSWORD', validators=[DataRequired()])
+        submit = SubmitField('Log in with eventmemo')
+
+
+    class EditProfile(FlaskForm):
+        username = StringField('USERNAME', validators=[DataRequired()])
+        email = StringField('EMAIL', validators=[DataRequired()])
+        password = PasswordField('PASSWORD', validators=[DataRequired()])
+        profil_pic = FileField('')
+        submit = SubmitField('save changes')
+
+
+    class Sign_up(FlaskForm):
+        username = StringField('USERNAME', validators=[DataRequired()])
+        email = StringField('EMAIL', validators=[DataRequired()])
+        password_hash = PasswordField('PASSWORD', validators=[DataRequired(), EqualTo(
+                'password_hash2', message='Passwords Must Match')])
+        password_hash2 = PasswordField(
+                'CONFIRM PASSWORD', validators=[DataRequired()])
+        profil_pic = FileField('Profile Pic')
+        submit = SubmitField('Create eventmemo account')
+
+
+    class PostForm(FlaskForm):
+        title = StringField("Title", validators=[DataRequired()])
+        start = DateTimeLocalField(
+          "Start", format="%Y-%m-%dT%H:%M", validators=[DataRequired()])
+        end = DateTimeLocalField("End", format="%Y-%m-%dT%H:%M", validators=[DataRequired()])
+        slug = StringField("Slug", validators=[DataRequired()])
+        place = StringField('location', validators=[DataRequired()])
+        image = FileField('Image')
+        submit = SubmitField("Upload event")
+
+    class EditEventForm(FlaskForm):
+        title = StringField('Title', validators=[DataRequired()])
+        start = DateTimeLocalField(
+          "Start", format="%Y-%m-%dT%H:%M", validators=[DataRequired()])
+        end = DateTimeLocalField("End", format="%Y-%m-%dT%H:%M", validators=[DataRequired()])
+        slug = StringField('Slug', validators=[DataRequired()])
+        place = StringField('location', validators=[DataRequired()])
+        image = FileField('Image')
+        submit = SubmitField('Save Changes')
+
+    class CommentForm(FlaskForm):
+        text = StringField(validators=[
+        InputRequired(), Length(min=4, max=26)], render_kw={"placeholder": "comment some"})
+
+        submit = SubmitField('Comment')
+
+
 
     with app.app_context():
         db.create_all()
